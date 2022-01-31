c069fe689057ffff40e681e3a5c896be7f922a4e
Taras Koshletskyi <taras.koshletskyi@health-union.com>
2022-01-31 18:49:15 +0200

    fix bugs

---
+++ not_island/qq.py
@@ -0,0 +1,14 @@
+def most_frequent(data: list) -> str:
+    d = {x: data.count(x) for x in set(data)}
+    return sorted(d, key=d.get, reverse=True)[0]
+
+
+if __name__ == "__main__":
+    # These "asserts" using only for self-checking and not necessary for auto-testing
+    print("Example:")
+    print(most_frequent(["a", "b", "c", "a", "b", "a"]))
+
+    assert most_frequent(["a", "b", "c", "a", "b", "a"]) == "a"
+
+    assert most_frequent(["a", "a", "bi", "bi", "bi"]) == "bi"
+    print("Done")
--- test8.py
+++
@@ -1,8 +0,0 @@
-students = {
-  'Theodore': 19,
-  'Roxanne': 22,
-  'Mathew': 21,
-  'Betty': 20
-}
-
-print(max(students, key = students.get), min(students, key = students.get))
